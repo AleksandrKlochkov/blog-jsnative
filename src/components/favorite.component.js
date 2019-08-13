1039c4c4c6a5c029067f071c6a97392a6aee3071
Alex <surfer.ru@mail.ru>
2019-08-13 15:57:35 +0400

    adding new features

--- src/components/favorite.component.js
+++ src/components/favorite.component.js
@@ -1,8 +1,54 @@
 import { Component } from "../core/component"
+import { EventEmitter } from "events";
+import { apiService } from "../services/api.service";
+import { renderPost } from '../templates/post.template'
 
 export class FavoriteComponent extends Component {
-    constructor(id){
+    constructor(id, {loader}){
         super(id)
+
+        this.loader = loader
     }
 
+    init() {
+        this.$el.addEventListener('click', linkClickHandler.bind(this))
+    }
+
+    onShow() {
+        const favorites = JSON.parse(localStorage.getItem('favorites'))
+        const html = renderList(favorites)
+        this.$el.insertAdjacentHTML('afterbegin', html)
+    }
+
+    onHide(){
+        this.$el.innerHTML = ''
+    }
+
+}
+
+async function linkClickHandler(event) {
+    event.preventDefault()
+
+    if(event.target.classList.contains('js-link')){
+        const postId = event.target.textContent
+        this.$el.innerHTML=''
+        this.loader.show()
+        const post = await apiService.fetchPostById(postId)
+        this.loader.hide()
+        const html = renderPost(post, {widthButton: false})
+        this.$el.insertAdjacentHTML('afterbegin', html)
+    }
+}
+
+function renderList(list=[]){
+    console.log(list)
+    if(list.length) {
+        return `
+            <ul>
+                ${list.map(i =>  `<li><a href="#" class="js-link">${i}</a></li>`).join(' ')}
+            </ul>
+        `
+    }
+
+    return `<p class="center">Вы пока ничего не добавили</p>`
 }
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -29,6 +29,17 @@ class ApiService {
         }
     }
 
+    async fetchPostById(id){
+        try{
+            const request = new Request(`${this.url}/posts/${id}.json`, {
+                method: 'get'
+            }) 
+            return useRequest(request)
+        }catch(error){
+            console.error(error)
+        }
+    }
+
 
 
 }
